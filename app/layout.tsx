457648f92dbc11b688d38594210e9fa3f9eb4059
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2025-01-15 01:58:35 +0200

    Update google site verification headers

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export const metadata: Metadata = {
   title: siteConfig.name,
   description: siteConfig.description,
   metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL ?? siteConfig.url),
+  other: {
+    "google-site-verification": "-YtHlJA-AGFUUWt8WP7SgCsiGXwwmpDlzuufVfkDRc0",
+  },
 };
 
 export const viewport: Viewport = {
